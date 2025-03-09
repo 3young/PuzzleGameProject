eebaf5dfd2f26930baec30a7e56aa0652d66ef62
3young <117700026+3young@users.noreply.github.com>
2025-03-10 01:40:47 +0900

    25-03-09 Puzzle-2
    5 이펙트 만들기
    6 게임 오버
    7 오브젝트 풀링
    8 사운드
    9 점수
    10 재시작

--- Puzzleproject/Assets/Scripts/Dongle.cs
+++ Puzzleproject/Assets/Scripts/Dongle.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class Dongle : MonoBehaviour
 {
     public GameManager manager;
+    public ParticleSystem effect;
     public int level;
 
     Rigidbody2D rigid;
     Animator anim;
+    SpriteRenderer spriteRenderer;
     CircleCollider2D circleCollider;
 
     bool isDrag;
     bool isMerge;
+    bool isAttach;
+    float deadTime;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         circleCollider = GetComponent<CircleCollider2D>();
     }
 
@@ -43,7 +48,20 @@ public class Dongle : MonoBehaviour
         if (isDrag)
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePos.y = 8;
+            // x축 최대값 적용
+            float LeftBorder = -4.2f + transform.localScale.x / 2;
+            float RightBorder = 4.2f - transform.localScale.x / 2;
+
+            if (mousePos.x < LeftBorder)
+            {
+                mousePos.x = LeftBorder;
+            }
+            else if (mousePos.x > RightBorder)
+            {
+                mousePos.x = RightBorder;
+            }
+
+            mousePos.y = 7;
             mousePos.z = 0;
             transform.position = Vector3.Lerp(transform.position, mousePos, 0.1f);
         }
@@ -63,7 +81,26 @@ public class Dongle : MonoBehaviour
         rigid.simulated = true;
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isAttach = true;
+
+        StartCoroutine(AttachRoutine());
+    }
+
+    IEnumerator AttachRoutine()
+    {
+        if (!isAttach)
+            yield break;
+
+        isAttach = true;
+        manager.PlaySfx(GameManager.Sfx.Attach);
+
+        yield return new WaitForSeconds(0.2f);
+        isAttach = false;
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
     {
         // 충돌 상대편이 동글일 때
         if (collision.gameObject.tag == "Dongle")
@@ -103,6 +140,8 @@ public class Dongle : MonoBehaviour
         anim.SetInteger("Level", level+1);
         anim.SetBool("IsPlaying", false);
         StartCoroutine(ResetAnimation());
+        manager.PlaySfx(GameManager.Sfx.LevelUp);
+        EffectPlay();
 
         yield return new WaitForSeconds(0.3f);
         level++;
@@ -112,7 +151,7 @@ public class Dongle : MonoBehaviour
         isMerge = false;
     }
 
-    void Hide(Vector3 targetPos)
+    public void Hide(Vector3 targetPos)
     {
         // 잠금 ON
         isMerge = true;
@@ -122,6 +161,12 @@ public class Dongle : MonoBehaviour
         circleCollider.enabled = false;
         // 숨기기 코루틴 실행
         StartCoroutine("HideRoutine", targetPos);
+
+        // 게임 오버 시 이펙트 플레이
+        if (targetPos == Vector3.up * 100)
+        {
+            EffectPlay();
+        }
     }
 
     IEnumerator HideRoutine(Vector3 targetPos)
@@ -130,12 +175,80 @@ public class Dongle : MonoBehaviour
         while (timeCount < 20)
         {
             timeCount++;
-            transform.position = Vector3.Lerp(transform.position, targetPos, 0.5f);
+            // 상대가 있을 시
+            if (targetPos != Vector3.up * 100)
+            {
+                transform.position = Vector3.Lerp(transform.position, targetPos, 0.5f);
+            }
+            // 게임 오버 시
+            else if (targetPos == Vector3.up * 100)
+            {
+                transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, 0.2f);
+            }
             yield return null;
+
         }
+        // 점수 증가
+        manager.score += (int)Mathf.Pow(2, level);
         // 비활성화
         gameObject.SetActive(false);
         // 잠금 OFF
         isMerge = false;
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Finish")
+        {
+            // 데드타임 증가
+            deadTime += Time.deltaTime;
+            // 2초 이상 경과 시 빨간색으로 변경하여 경고
+            if (deadTime > 2)
+            {
+                spriteRenderer.color = new Color(0.9f, 0.2f, 0.2f);
+            }
+            // 5초 이상 경과 시 게임 오버
+            if (deadTime > 5)
+            {
+                manager.Result();
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Finish")
+        {
+            // 데드타임 및 색상 초기화
+            deadTime = 0;
+            spriteRenderer.color = Color.white;
+        }
+    }
+
+    void OnDisable()
+    {
+        // 동글 속성 초기화
+        level = 0;
+        deadTime = 0;
+
+        // 동글 위치, 크기, 회전값 초기화
+        transform.localPosition = Vector3.zero;
+        transform.localScale = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+
+        // 동글 물리 초기화
+        rigid.simulated = false;
+        rigid.velocity = Vector2.zero;
+        rigid.angularVelocity = 0f;
+        circleCollider.enabled = true;
+    }
+
+    void EffectPlay()
+    {
+        // 파티클 위치와 크기 설정
+        effect.transform.position = transform.position;
+        effect.transform.localScale = transform.localScale;
+        // 파티클 플레이
+        effect.Play();
     }
 }
--- Puzzleproject/Assets/Scripts/GameManager.cs
+++ Puzzleproject/Assets/Scripts/GameManager.cs
@@ -1,44 +1,137 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
+using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("-------[ Core ]")]
+    public int maxLevel;
+    public bool isOver;
+    public int score;
+
+    [Header("-------[ Object Pooling ]")]
     public GameObject donglePrefab;
     public Transform dongleGroup;
-    public Dongle lastDongle;
-    public int maxLevel;
+    public GameObject effectPrefab;
+    public Transform effectGroup;
+    [Range(1, 20)]
+    public int poolSize;
+    public List<Dongle> donglePool;
+    public List<ParticleSystem> effectPool;
+    Dongle lastDongle;
+    int poolCursor;
+
+    [Header("-------[ Audio ]")]
+    public AudioSource bgmPlayer;
+    public AudioSource[] sfxPlayers; // Change from AudioSource to AudioSource[]
+    public AudioClip[] sfxClips;
+    int sfxCursor;
+
+    [Header("-------[ UI ]")]
+    public GameObject line;
+    public GameObject floor; 
+    public GameObject startGroup;
+    public GameObject endGroup;
+    public Text scoreText;
+    public Text maxScoreText;
+    public Text subScoreText;
+
+    public enum Sfx { LevelUp, Next, Attach, Button, GameOver };
+
 
     void Awake()
     {
+        // 프레임 설정 (FPS 60)
         Application.targetFrameRate = 60;
+
+        // 오브젝트 풀 시작
+        donglePool = new List<Dongle>();
+        effectPool = new List<ParticleSystem>();
+        for(int index = 0; index < poolSize; index++)
+        {
+            MakeDongle(index);
+        }
+
+        // 최대 점수 설정
+        if(PlayerPrefs.HasKey("MaxScore"))
+        {
+            PlayerPrefs.SetInt("MaxScore", 0);
+        }
+
+        maxScoreText.text = PlayerPrefs.GetInt("MaxScore").ToString();
     }
 
-    void Start()
+    Dongle MakeDongle(int id)
     {
-        // 게임 시작 세팅
-        GameStart();
+        // 새로운 이펙트 생성 + 풀에 추가
+        GameObject instantEffect = Instantiate(effectPrefab, effectGroup);
+        instantEffect.name = "Effect_" + id;
+        ParticleSystem instantEffectParticle = instantEffect.GetComponent<ParticleSystem>();
+        effectPool.Add(instantEffectParticle);
+
+        // 새로운 동글 생성 (생성 -> 레벨 설정 -> 활성화) + 풀에 추가
+        GameObject instantDongle = Instantiate(donglePrefab, dongleGroup);
+        Dongle instantDongleLogic = instantDongle.GetComponent<Dongle>();
+        instantDongle.name = "Dongle_" + id;
+        instantDongleLogic.manager = this;
+        instantDongleLogic.effect = instantEffectParticle;
+        donglePool.Add(instantDongleLogic);
+
+        return instantDongleLogic;
     }
 
-    void GameStart()
+    Dongle GetDongle()
     {
+        for(int index = 0; index < donglePool.Count; index++)
+        {
+            poolCursor = (poolCursor + 1) % donglePool.Count;
+            if (!donglePool[poolCursor].gameObject.activeSelf)
+            {
+                return donglePool[poolCursor];
+            }
+        }
+
+        return MakeDongle(donglePool.Count);
+    }
+
+    public void GameStart()
+    {
+        // UI 컨트롤
+        startGroup.SetActive(false);
+        line.SetActive(true);
+        floor.SetActive(true);
+        scoreText.gameObject.SetActive(true);
+        maxScoreText.gameObject.SetActive(true);
+
+        // 버튼 효과음
+        PlaySfx(Sfx.Button);
+
+        // BGM 시작
+        bgmPlayer.Play();
+
+        // 동글 생성 시작
         Invoke("NextDongle", 1.5f);
     }
 
     void NextDongle()
     {
-        // 새로운 동글 생성 (생성 -> 레벨 설정 -> 활성화)
-        GameObject instant = Instantiate(donglePrefab, dongleGroup);
-        lastDongle = instant.GetComponent<Dongle>();
-        lastDongle.manager = this;
-        lastDongle.level = Random.Range(0, maxLevel); 
-        instant.SetActive(true);
+        if (isOver)
+            return;
+
+        // 다음 동글 가져오기
+        lastDongle = GetDongle();
+        lastDongle.level = Random.Range(0, maxLevel);
+        lastDongle.gameObject.SetActive(true);
 
         // 다음 동글 생성을 기다리는 코루틴
         StartCoroutine(WaitNext());
         // StartCoroutine("WaitNext");
 
+        // 효과음 재생
+        PlaySfx(Sfx.Next);
     }
 
     IEnumerator WaitNext()
@@ -49,7 +142,7 @@ public class GameManager : MonoBehaviour
             yield return null; // 한 프레임 쉼
         }
         
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(2.5f);
 
         // 다음 동글 생성 호출
         NextDongle();
@@ -57,14 +150,101 @@ public class GameManager : MonoBehaviour
 
     public void TouchDown()
     {
+        if (lastDongle == null)
+            return;
+
         // 동글 드래그
         lastDongle.Drag();
     }
 
     public void TouchUp()
     {
+        if (lastDongle == null)
+            return;
+
         // 동글 드랍 (변수 비우기)
         lastDongle.Drop();
         lastDongle = null;
+    }
+
+    public void Result()
+    {
+        // 게임 오버 및 결산
+        isOver = true;
+        bgmPlayer.Stop();
+
+        StartCoroutine("ResultRoutine");
+    }
+
+    IEnumerator ResultRoutine()
+    {
+        // 남아있는 동글들을 순차적으로 숨김
+        for (int index = 0; index < donglePool.Count; index++)
+        {
+            if (donglePool[index].gameObject.activeSelf)
+            {
+                donglePool[index].Hide(Vector3.up * 100);
+                yield return new WaitForSeconds(0.05f); 
+            }
+        }
+
+        yield return new WaitForSeconds(1f);
+        // 점수 적용
+        subScoreText.text = "점수 : " + scoreText.text;
+        // 최대 점수 갱신
+        int maxScore = Mathf.Max(PlayerPrefs.GetInt("MaxScore"), score);
+        PlayerPrefs.SetInt("MaxScore", maxScore);
+        // UI 띄우기
+        endGroup.SetActive(true);
+        // 효과음 재생
+        PlaySfx(Sfx.GameOver);
+    }
+
+    public void Reset()
+    {
+        // 효과음 재생
+        PlaySfx(Sfx.Button);
+        StartCoroutine(ResetRoutine());
+    }
+
+    IEnumerator ResetRoutine()
+    {
+        yield return new WaitForSeconds(1.0f);
+        // 장면 다시 불러오기
+        SceneManager.LoadScene(0);
+    }
+
+    public void PlaySfx(Sfx type)
+    {
+        // SFX 플레이어 커서 이동
+        sfxCursor = (sfxCursor + 1) % sfxPlayers.Length;
+
+        // 효과음 종류에 따라 클립 설정
+        switch (type)
+        {
+            case Sfx.LevelUp:
+                sfxPlayers[sfxCursor].clip = sfxClips[Random.Range(0, 3)];
+                break;
+            case Sfx.Next:
+                sfxPlayers[sfxCursor].clip = sfxClips[3];
+                break;
+            case Sfx.Attach:
+                sfxPlayers[sfxCursor].clip = sfxClips[4];
+                break;
+            case Sfx.Button:
+                sfxPlayers[sfxCursor].clip = sfxClips[5];
+                break;
+            case Sfx.GameOver:
+                sfxPlayers[sfxCursor].clip = sfxClips[6];
+                break;
+        }
+
+        // 효과음 재생
+        sfxPlayers[sfxCursor].Play();
+    }
+
+    void LateUpdate()
+    {
+        scoreText.text = score.ToString();  
     }
 }
